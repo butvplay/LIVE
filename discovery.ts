d19ecccb0158ad44b0f07f253f9df18b6afdd871
butvplay <marcodgg@outlook.com>
2016-10-27 13:48:30 -0500

    Update discovery.ts

--- discovery.ts
+++ discovery.ts
@@ -3,4 +3,4 @@
 #EXT-X-TARGETDURATION:5
 #EXT-X-MEDIA-SEQUENCE:377034
 #EXTINF:4.004,
-http://cinestrenostv.tv/canales/documentales/discoverychannel.php
+http://kdsajkldsadjsad77576dsa99saghdshhdds.sitenow.me/live/discoverydino_9dm3fgv/chunklist.m3u8
