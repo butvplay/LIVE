158e815dc28fdc4f5be572dc526a43cee925055c
butvplay <marcodgg@outlook.com>
2016-11-07 16:10:36 -0500

    Update PROFILE03.ts

--- CANAL102/PROFILE03.ts
+++ CANAL102/PROFILE03.ts
@@ -1,3 +1,3 @@
 #EXTM3U
 #EXTINF:-1  ,CANAL DE LAS ESTRELLAS
-http://oneplay.tv/streaming/reproducir/325.m3u8?OPToken=74e98708dfb09fdc8efaca7ae35c10bf
+http://oneplay.tv/embed/?i=70&n=Tiin&w=100%25&h=100%25&token=74e98708dfb09fdc8efaca7ae35c10bf
